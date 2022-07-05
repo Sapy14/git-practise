b09054af3568255721e85279c6a71d5ce605027b
Sapy Sharma <sapy.sharma14@gmail.com>
2022-07-05 18:47:33 +0530

    Updated

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .import views
 from rest_framework import routers
 
-router=routers.DefaultRouter()
+router=routers.DefaultRouter
 router.register('question',views.ViewApi)
 
 urlpatterns = [
